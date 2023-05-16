ca1ce0c32736bd2636d98e1289e341bf77d52c0b
Kotaro Shimizu <nekottyo@gmail.com>
2023-05-16 16:11:21 +0900

    update (#17)

--- test.sh
+++ test.sh
@@ -4,6 +4,7 @@ git checkout -B test
 git commit -m 'test' --allow-empty
 git push origin test
 gh pr create --fill -l 'bump:patch'
-gh pr merge --squash --auto
+gh pr merge --squash --auto -d
 git checkout main
+git sync
 git dmb
